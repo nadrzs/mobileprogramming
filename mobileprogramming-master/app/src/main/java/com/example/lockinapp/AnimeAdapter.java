f2fa2c2d86f1d38c5d6f66e55e99233020bc7816
Nadd <42337677+nadrzs@users.noreply.github.com>
2020-12-02 21:25:09 +0700

    Add files via upload

---
+++ mobileprogramming-master/app/src/main/java/com/example/lockinapp/AnimeAdapter.java
@@ -0,0 +1,94 @@
+package com.example.lockinapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.lockinapp.Model.AnimItem;
+import com.squareup.picasso.Picasso;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class AnimeAdapter extends RecyclerView.Adapter<AnimeAdapter.MyViewHolder> {
+
+    List<AnimItem> animeList;
+
+    public AnimeAdapter(List<AnimItem> animItems) {
+        this.animeList = animItems;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.anime_layout, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        final AnimItem item = animeList.get(position);
+
+        DecimalFormat members = (DecimalFormat) DecimalFormat.getNumberInstance();
+        DecimalFormatSymbols formatSymbols = new DecimalFormatSymbols();
+        formatSymbols.setGroupingSeparator('.');
+
+        members.setDecimalFormatSymbols(formatSymbols);
+
+        Picasso.get().load(item.getImageUrl()).into(holder.animeImage);
+        holder.animeTitle.setText(item.getTitle());
+        holder.animeType.setText(String.format("%s (%d)", item.getType(), item.getEpisodes()));
+        holder.animeStartEnd.setText(String.format("%s - %s", item.getStart_date(), item.getEnd_date()));
+        holder.animeMembers.setText(String.format("%s Members", members.format(item.getMembers())));
+        holder.animeRating.setText(String.valueOf(item.getScore()));
+
+//        holder.itemView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//            }
+//        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return animeList.size();
+    }
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.anime_image)
+        ImageView animeImage;
+        @BindView(R.id.anime_title)
+        TextView animeTitle;
+        @BindView(R.id.anime_type)
+        TextView animeType;
+        @BindView(R.id.anime_startEnd)
+        TextView animeStartEnd;
+        @BindView(R.id.anime_members)
+        TextView animeMembers;
+        @BindView(R.id.anime_rating)
+        TextView animeRating;
+        @BindView(R.id.rating_image)
+        ImageView ratingImage;
+
+        public MyViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, itemView);
+        }
+
+    }
+}
