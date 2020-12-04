dc8c58ab6ed5ee82f88039334b8b819cd0f70807
Marsa Ariqi Gustiandza <marsa.gustiandza@binus.ac.id>
2020-12-05 02:37:51 +0700

    Contact Us add Map

--- app/src/main/java/com/example/lockinapp/ContactUs.java
+++ app/src/main/java/com/example/lockinapp/ContactUs.java
@@ -1,14 +1,37 @@
 package com.example.lockinapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
-public class ContactUs extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class ContactUs extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng Anima = new LatLng(-6.200059163004611, 106.78536584032943);
+        map.addMarker(new MarkerOptions().position(Anima).title("Anima Ltd"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(Anima));
     }
 }
